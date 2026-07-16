0d6dae7c86693f5bf7236be99cd6fba0a5c333c3
johnatanduarte <johnatanduartefranco95@gmail.com>
2026-07-16 09:47:16 -0300

    fix: resolve tela branca no navegador e ajusta imports do Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Cadastro from "./pages/Cadastro/Cadastro.jsx";
 import "./App.css";
 import EsqueceuSenha from "./pages/esqueceuSenha/EsqueceuSenha";
 import Inicio from "./pages/Inicio/Inicio.jsx";
+import Login from "./pages/Login/Login.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/Cadastro" element={<Cadastro />} />
         <Route path="/EsqueceuSenha" element={<EsqueceuSenha />} />
         <Route path="/Inicio" element={<Inicio />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </>
   );
